ae2ef90632d270ba35d9c5c52185ab13f723c9db
EggPool <dev@EggPool.net>
2019-08-02 09:44:14 +0200

    Formatting

--- RPCServer/rpcwallet.py
+++ RPCServer/rpcwallet.py
@@ -23,7 +23,7 @@ from rpckeys import Key
 __version__ = "0.0.6"
 
 RE_RSA_ADDRESS = re.compile(r"[abcdef0123456789]{56}")
-# TODO: improve that ECDSA one
+# TODO: Use Polysign now.
 RE_ECDSA_ADDRESS = re.compile(r"^Bis")
 
 app_log = getLogger("tornado.application")
@@ -34,23 +34,34 @@ class Wallet:
     Handles a .wallet directory, with accounts, addresses, keys and wallet encryption/backup.
     Content is stored as json, within several dir to limit the files in each directory
     """
-    # Warning: make sure this is thread safe as it will be called from multiple threads. Use queues and locks when needed.
-    # TODO
+
+    # Warning: make sure this is thread safe as it will be called from multiple threads.
+    # Use queues and locks when needed.
 
     # TODO: When first iteration ok, process all docstrings
 
-    __slots__ = ('path', 'verbose', 'encrypted', 'locked', 'passphrase', 'IV','index', 'key', 'address_to_account')
+    __slots__ = (
+        "path",
+        "verbose",
+        "encrypted",
+        "locked",
+        "passphrase",
+        "IV",
+        "index",
+        "key",
+        "address_to_account",
+    )
     # TODO: those properties should be converted to _protected later on.
 
-    def __init__(self, path='.wallet', verbose=False):
+    def __init__(self, path=".wallet", verbose=False):
         self.path = path
         self.verbose = verbose
         self.encrypted = False
         self.locked = False
-        self.passphrase = ''
+        self.passphrase = ""
         self.index = None
         self.address_to_account = {}
-        self.IV = 16 * '\x00'
+        self.IV = 16 * "\x00"
         if not os.path.exists(path):
             if self.verbose:
                 app_log.warning("Path {} does not exist, creating".format(path))
@@ -60,21 +71,23 @@ class Wallet:
         self.load()
         if self.verbose:
             app_log.info(self.index)
-        #self.reindex()
+        # self.reindex()
 
     def load(self):
         """
         Loads the current wallet state or init if the dir is empty.
         """
         # At this point the dir exists.
-        index_fname = self.path+'/index.json'
-        rindex_fname = self.path+'/rindex.json'
+        index_fname = self.path + "/index.json"
+        rindex_fname = self.path + "/rindex.json"
         if not os.path.exists(index_fname):
             if self.verbose:
-                app_log.warning("{} does not exist, creating default".format(index_fname))
+                app_log.warning(
+                    "{} does not exist, creating default".format(index_fname)
+                )
                 # Default index file
-                self.index = {'version': __version__, 'encrypted':False}
-                with open(index_fname, 'w') as outfile:
+                self.index = {"version": __version__, "encrypted": False}
+                with open(index_fname, "w") as outfile:
                     json.dump(self.index, outfile)
                 # Inverted index
                 self.address_to_account = {}
@@ -97,69 +110,73 @@ class Wallet:
             for afile in files:
                 # check if this is a json file
                 ext = os.path.splitext(afile)[-1].lower()
-                if ext != '.json':
+                if ext != ".json":
                     continue
                 # Avoid the indexes
-                if afile.lower() in ('index.json', 'rindex.json') :
+                if afile.lower() in ("index.json", "rindex.json"):
                     continue
                 # io is used here to avoid cross platform issues with UTF-8 BOM.
-                with io.open(os.path.join(root,afile), 'r', encoding='utf-8-sig') as json_file:
+                with io.open(
+                    os.path.join(root, afile), "r", encoding="utf-8-sig"
+                ) as json_file:
                     try:
                         json_contents = json_file.read()
                         res = json.loads(json_contents)
                         account = os.path.splitext(afile)[0]
-                        if 'default' == account:
-                            account = ''
+                        if "default" == account:
+                            account = ""
                         yield (account, res)
                     except Exception as e:
                         if self.verbose:
-                            app_log.warning("Possible error {} on file {}".format(e, afile))
+                            app_log.warning(
+                                "Possible error {} on file {}".format(e, afile)
+                            )
 
     def _save_rindex(self):
         """
         Sync our reverse index to file
         """
-        rindex_fname = self.path+'/rindex.json'
+        rindex_fname = self.path + "/rindex.json"
         # TODO: Lock
-        with open(rindex_fname, 'w') as outfile:
+        with open(rindex_fname, "w") as outfile:
             json.dump(self.address_to_account, outfile)
 
-    def _check_account_name(self, account=''):
+    def _check_account_name(self, account=""):
         """
         Raise an exception if account name does not comply
         An account is 2-128 characters long, and may only contains the Base64 Character set.
         """
-        if account == '':
+        if account == "":
             # empty if default account
             return True
         if len(account) < 2 or len(account) > 128:
             raise InvalidAccountName
         # Only b64 charset
-        if re.search('[^a-zA-Z0-9\+\/]', account) :
+        if re.search("[^a-zA-Z0-9+/]", account):
             raise InvalidAccountName
 
-    def _get_account(self, account=''):
+    def _get_account(self, account=""):
         """
         Returns a dict with the given account info.
         """
         self._check_account_name(account)
-        if '' == account or 'default' == account:
-            fname = self.path+'/default.json'
+        if "" == account or "default" == account:
+            fname = self.path + "/default.json"
             path = self.path
         else:
             adir = account[:2]
-            path = self.path+'/'+adir
-            fname = path+'/'+account+'.json'
+            path = self.path + "/" + adir
+            fname = path + "/" + account + ".json"
         if not os.path.isfile(fname):
             if self.verbose:
                 app_log.info("{} does not exist, creating default".format(fname))
             # Default account file
-            self.key.generate() # This takes some time.
-            res = {'encrypted':False, 'addresses':[self.key.as_list]}
+            self.key.generate()  # This takes some time.
+            res = {"encrypted": False, "addresses": [self.key.as_list]}
             # and save account
             if not os.path.exists(path):
                 os.mkdir(path)
-            with open(fname, 'w') as outfile:
+            with open(fname, "w") as outfile:
                 json.dump(res, outfile)
             # update reverse index
             self.address_to_account[self.key.address] = account
@@ -169,26 +186,28 @@ class Wallet:
                 res = json.load(json_file)
         return res
 
-    def _save_account(self, account_dict, account=''):
+    def _save_account(self, account_dict, account=""):
         """
         Saves account info back to disk
         """
         # TODO: lock
         self._check_account_name(account)
-        if '' == account:
-            fname = self.path+'/default.json'
+        if "" == account:
+            fname = self.path + "/default.json"
             path = self.path
         else:
             adir = account[:2]
-            path = self.path+'/'+adir
-            fname = path+'/'+account+'.json'
+            path = self.path + "/" + adir
+            fname = path + "/" + account + ".json"
         if not os.path.exists(path):
             os.mkdir(path)
-        with open(fname, 'w') as outfile:
+        with open(fname, "w") as outfile:
             json.dump(account_dict, outfile)
         return True
 
-    def make_unsigned_transaction(self, from_address, to_address, amount=0, data='', timestamp = 0):
+    def make_unsigned_transaction(
+        self, from_address, to_address, amount=0, data="", timestamp=0
+    ):
         """
         :param from_address:
         :param to_address:
@@ -198,17 +217,26 @@ class Wallet:
         :return: List. An unsigned transaction, mempool format
         """
         if float(timestamp) <= 0:
-            timestamp = '%.2f' % time.time()
+            timestamp = "%.2f" % time.time()
         else:
             # correct format %.2f is critical for signature validity
-            timestamp = '%.2f' % float(timestamp)
-        amount = '%.8f' % float(amount)
-        keep = '0'
-        signature = ''
-        public_key_hashed = ''
+            timestamp = "%.2f" % float(timestamp)
+        amount = "%.8f" % float(amount)
+        keep = "0"
+        signature = ""
+        public_key_hashed = ""
         if len(data) > 100000:
             data = data[:100000]
-        return [timestamp, str(from_address), str(to_address), amount, signature, public_key_hashed, keep, str(data)]
+        return [
+            timestamp,
+            str(from_address),
+            str(to_address),
+            amount,
+            signature,
+            public_key_hashed,
+            keep,
+            str(data),
+        ]
 
     def sign_transaction(self, transaction):
         """
@@ -221,7 +249,9 @@ class Wallet:
         if float(transaction[3]) < 0:
             raise NegativeAmount
         # signed_part has to be a tuple, or the signature won't match
-        signed_part = tuple(transaction[:4] + transaction[6:8]) # This removes signature and "hashed" pubkey
+        signed_part = tuple(
+            transaction[:4] + transaction[6:8]
+        )  #  This removes signature and "hashed" pubkey
         # Find the keys and init the crypto thingy
         the_key = Key()
         the_key.from_list(self._get_keys_for_address(address))
@@ -229,9 +259,9 @@ class Wallet:
         signature_enc = the_key.sign(signed_part, base64_output=True)
         public_key_hashed = the_key.hashed_pubkey
         signed = list(transaction)
-        signed[4] = str(signature_enc.decode('utf-8'))
+        signed[4] = str(signature_enc.decode("utf-8"))
         signed[5] = public_key_hashed
-        #txid = signature_enc[:56]
+        # txid = signature_enc[:56]
         return signed
 
     def reindex(self):
@@ -243,7 +273,7 @@ class Wallet:
         self.address_to_account = {}
         for account_name, account_details in self._parse_accounts():
             try:
-                for address in account_details['addresses']:
+                for address in account_details["addresses"]:
                     self.address_to_account[address[0]] = account_name
             except:
                 pass
@@ -256,12 +286,14 @@ class Wallet:
             addresses.extend(self.get_addresses_by_account(account_name))
         return addresses
 
-    def get_account_address(self, anaccount=''):
+    def get_account_address(self, anaccount=""):
         """
         returns the default address of the given account
         """
-        account = self._get_account(anaccount) # This will handle address creation if doesn't exists yet.
-        addresses = account['addresses'][0]
+        account = self._get_account(
+            anaccount
+        )  # This will handle address creation if doesn't exists yet.
+        addresses = account["addresses"][0]
         # Addresses is on fact [address,privkey,pubkey]
         # with privkey encrypted if wallet is.
         return addresses[0]
@@ -290,7 +322,7 @@ class Wallet:
         try:
             # print(self.address_to_account[address])
             account = self._get_account(self.address_to_account[address])
-            for keys in account['addresses']:
+            for keys in account["addresses"]:
                 # keys is [address, encrypted, privkey, pubkey]
                 if keys[0] == address:
                     return keys
@@ -319,15 +351,15 @@ class Wallet:
         :return: dict
         """
         # Format check
-        info = {'address': address, 'valid': self.address_is_valid(address)}
+        info = {"address": address, "valid": self.address_is_valid(address)}
         # If this address in our wallet?
         if address in self.address_to_account:
             account_name = self.address_to_account[address]
-            info['ismine'] = True
-            info['account'] = account_name
+            info["ismine"] = True
+            info["account"] = account_name
         else:
-            info['ismine'] = False
-            info['account'] = None
+            info["ismine"] = False
+            info["account"] = None
         return info
 
     def dump_privkey(self, address):
@@ -337,7 +369,7 @@ class Wallet:
         keys = self._get_keys_for_address(address)
         return keys[2]
 
-    def import_privkey(self, privkey, account_name='', rescan=False):
+    def import_privkey(self, privkey, account_name="", rescan=False):
         """
         :param privkey:
         :param account_name:
@@ -348,35 +380,37 @@ class Wallet:
         the_key = Key(verbose=self.verbose)
         the_key.from_privkey(privkey)
         account = self._get_account(account_name)
-        account['addresses'].append(the_key.as_list)
+        account["addresses"].append(the_key.as_list)
         # update reverse index
         self.address_to_account[the_key.address] = account_name
         self._save_rindex()
         self._save_account(account, account_name)
         return None
 
-    def get_new_address(self, anaccount=''):
+    def get_new_address(self, an_account=""):
         """
         returns a new address for the given account
         """
-        account_dict = self._get_account(anaccount) # This will handle address creation if doesn't exists yet.
-        self.key.generate() # This takes some time.
+        account_dict = self._get_account(
+            an_account
+        )  # This will handle address creation if doesn't exists yet.
+        self.key.generate()  # This takes some time.
 
-        account_dict['addresses'].append(self.key.as_list)
-        self._save_account(account_dict, account=anaccount)
+        account_dict["addresses"].append(self.key.as_list)
+        self._save_account(account_dict, account=an_account)
         # update reverse index
-        self.address_to_account[self.key.address] = anaccount
+        self.address_to_account[self.key.address] = an_account
         self._save_rindex()
         return self.key.address
 
-    def get_addresses_by_account(self, anaccount=''):
+    def get_addresses_by_account(self, an_account=""):
         """
         returns the list of addresses of the given account
         """
-        account = self._get_account(anaccount)
-        return [address[0] for address in account['addresses']]
+        account = self._get_account(an_account)
+        return [address[0] for address in account["addresses"]]
 
-    def backup_wallet(self, afilename='bwallet.zip'):
+    def backup_wallet(self, afilename="bwallet.zip"):
         """
         Saves the whole wallet directory in a zip or tgz archive
         """
@@ -385,7 +419,7 @@ class Wallet:
         if not os.path.exists(backup_path):
             raise InvalidPath
         # Open a zipfile for writing - afilename is the full path and filename of where to save.
-        wallet_zip = zipfile.ZipFile(afilename, 'w', zipfile.ZIP_DEFLATED)
+        wallet_zip = zipfile.ZipFile(afilename, "w", zipfile.ZIP_DEFLATED)
         # Walk all files and dirs and add to zipfile - self.path is wallet directory
         for root, dirs, files in os.walk(self.path):
             for file in files:
@@ -393,7 +427,7 @@ class Wallet:
         wallet_zip.close()
         return True
 
-    def dump_wallet(self, afilename='dump.txt', version='n/a'):
+    def dump_wallet(self, afilename="dump.txt", version="n/a"):
         """
         Sends back a list of all privkeys for the wallet.
         """
@@ -404,27 +438,38 @@ class Wallet:
         if not os.path.exists(afilename):
             if self.verbose:
                 app_log.info("{} does not exist, creating".format(afilename))
-        with open(afilename, 'w') as outfile:
+        with open(afilename, "w") as outfile:
             # Write basic output
             outfile.write("# Wallet dump created by bismuthd {} \n".format(version))
-            outfile.write("# * Created on {} \n".format(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')))
+            outfile.write(
+                "# * Created on {} \n".format(
+                    datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                )
+            )
             if self.encrypted:
-                outfile.write('# * Wallet is Encrypted\n')
+                outfile.write("# * Wallet is Encrypted\n")
             else:
-                outfile.write('# * Wallet is Unencrypted\n')
+                outfile.write("# * Wallet is Unencrypted\n")
             """ @TODO add block information like:
-                # * Best block at time of backup was 227221 (0000000026ede4c10594af8087748507fb06dcd30b8f4f48b9cc463cabc9d767),
+                # * Best block at time of backup was 227221 
+                (0000000026ede4c10594af8087748507fb06dcd30b8f4f48b9cc463cabc9d767),
                 #   mined on 2014-04-29T21:15:07Z
             """
             for account_name, account_details in self._parse_accounts():
                 try:
-                    for address in account_details['addresses']:
+                    for address in account_details["addresses"]:
                         """
                             Output format:
                             privkey1 RESERVED account=account1 addr=address1
                             - RESERVED is for timestamp later
                         """
-                        outfile.write("{} RESERVED account={} addr={}\n".format(address[2].replace('\n','').replace('\r',''), account_name, address[0]))
+                        outfile.write(
+                            "{} RESERVED account={} addr={}\n".format(
+                                address[2].replace("\n", "").replace("\r", ""),
+                                account_name,
+                                address[0],
+                            )
+                        )
                 except:
                     # Silently ignore
                     pass
